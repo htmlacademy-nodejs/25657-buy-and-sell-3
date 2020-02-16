8d6bcfa94c27fab8c1d88e70d78b3477aac1c7e5
Eugeno <eugeneath@yandex.ru>
2020-02-16 14:40:11 +0700

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const fs = require(`fs`);
-const util = require(`util`);
+const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 const {
   getRandomInt,
@@ -11,45 +10,6 @@ const {
 const DEFAULT_COUNT = 1;
 const MAXIMUM_NUMBER_SENTENCES = 5;
 const FILE_NAME = `mock.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга.`,
-  `Продам новую приставку Sony Playstation 5.`,
-  `Продам отличную подборку фильмов на VHS.`,
-  `Куплю антиквариат.`,
-  `Куплю породистого кота.`,
-  `Продам коллекцию журналов «Огонёк».`,
-  `Отдам в хорошие руки подшивку «Мурзилка».`,
-  `Продам советскую посуду. Почти не разбита.`,
-  `Куплю детские санки.`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `Две страницы заляпаны свежим кофе.`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  `Кажется, что это хрупкая вещь.`,
-  `Мой дед не мог её сломать.`,
-  `Кому нужен этот новый телефон, если тут такое...`,
-  `Не пытайтесь торговаться. Цену вещам я знаю.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
 
 const OfferType = {
   offer: `offer`,
@@ -70,27 +30,37 @@ const getPictureFileName = (randomInt) => {
   return `item${(`0${randomInt}`).substr(-(PictureRestrict.toString()).length)}.jpg`;
 };
 
-const generateOffers = (count) => (
-  Array(count).fill({}).map(() => ({
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
-    description: shuffle(SENTENCES).slice(0, getRandomInt(1, MAXIMUM_NUMBER_SENTENCES)).join(` `),
+const readFileInfo = async (fileName) => {
+  try {
+    return (await fs.readFile(fileName, `utf-8`)).split(/\n/).filter(str => str !== '');
+  } catch (error) {
+    return console.error(chalk.red(`Can't read file ${fileName}`));
+  }
+};
+
+const generateOffers = async (count) => {
+  const sentences = await readFileInfo('data/sentences.txt');
+  const titles = await readFileInfo('data/titles.txt');
+  const categories = await readFileInfo('data/categories.txt');
+  return Array(count).fill({}).map(() => ({
+    category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
+    description: shuffle(sentences).slice(0, getRandomInt(1, MAXIMUM_NUMBER_SENTENCES)).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.min, SumRestrict.max),
-  }))
-);
+  }));
+};
 
 module.exports = {
   name: `--generate`,
   async run(args) {
     const [count] = args;
     const countOffer = Math.max(parseInt(count, 10) || DEFAULT_COUNT, DEFAULT_COUNT);
-    const content = JSON.stringify(generateOffers(countOffer));
-    const writeFile = util.promisify(fs.writeFile);
+    const content = JSON.stringify(await generateOffers(countOffer));
 
     try {
-      await writeFile(FILE_NAME, content);
+      await fs.writeFile(FILE_NAME, content);
       return console.info(chalk.green(`Operation success. File created.`));
     } catch (error) {
       return console.error(chalk.red(`Can't write data to file...`));
